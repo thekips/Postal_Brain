475faef54fa71e76b209e598eaa75e5b8877a7eb
ekipszhang(张思哲) <ekipszhang@tencent.com>
2021-07-14 17:45:36 +0800

    update heatmap

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -1,12 +1,19 @@
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-import random
-
-from seaborn.distributions import histplot
+from matplotlib.backends.backend_agg import FigureCanvas
 
 x = np.random.random(100) + 112.5 
 y = np.random.random(100) + 22.5
 
-plt.scatter(x, y, c=(1, 0, 0))
+fig1 = plt.figure(1)
+sns.kdeplot(x=x, y=y, shade=True)
+img1 = np.array(fig1.canvas.get_renderer()._renderer)
+print(img1.shape)
+
+fig2 = plt.figure(2)
+sns.scatterplot(x=x, y=y)
+img2= np.array(fig1.canvas.get_renderer()._renderer)
+print(img2.shape)
+
 plt.show()
